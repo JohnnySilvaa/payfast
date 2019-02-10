171118ef764ed4c5cf2a2f8ba611047de793358f
Johnny Silva <johnnysilva_estagio@hotmail.com>
2019-02-10 16:33:06 -0200

    curl

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,17 +1,16 @@
 //página inicial
 module.exports = (app) => {
-    app.get('/pagamentos', function(req, resp){
 
-        resp.send(`
-                    <html>
-                        <head>
-                            <meta charset="utf-8">
-                        </head>
-                        <body>
-                            <h1>Pagamentos</h1>
-                        </body>
-                    </html>
-                `);
+    app.get('/pagamentos', function(req, resp){
+        resp.send('Ok');
     });
-               
+
+    module.exports = (app) => {
+            app.post('/pagamentos/pagamento', function(req, resp){
+                    const pagamento= req.body;
+                    console.log(pagamento);
+                    resp.send('Ok');
+                });
+            }
+            
 }
